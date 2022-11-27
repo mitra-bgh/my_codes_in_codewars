7d45379f1e2f65914861b5206384f9a9c1ac824a
mitra-bgh <57005174+mitra-bgh@users.noreply.github.com>
2022-11-27 15:13:35 +0330

    Update k6.Reverse_every_other_word_in_the_string.py

--- k6.Reverse_every_other_word_in_the_string.py
+++ k6.Reverse_every_other_word_in_the_string.py
@@ -1,19 +1,14 @@
 #Reverse every other word in the string
 def reverse_alternate(string):
-    '''new_s = string.split()
+    new_s = string.split()
     result = ''
     for i in range(len(new_s)):
         if i%2 != 0:
             result += new_s[i][::-1] + " "
         else:
             result += new_s[i]+ " "
-    return result.strip()'''
+    return result.strip()
 
-    return " ".join(s[::-1] if i%2 else s for i,s in enumerate(string.split()))
-
-obj = reverse_alternate("Reverse this string, please!") #, "Reverse siht string, !esaelp")
+#call the function
+obj = reverse_alternate("Reverse this string, please!")
 print(obj)
-
-# Notes:
-# enumerate()
-# prog in one line
